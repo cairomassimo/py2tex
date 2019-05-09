75562cda38e94d2a1379ff347aa2e2e29952f1f7
Gemma Martini <martini.gemma3@gmail.com>
2019-05-09 17:14:35 +0200

    updated examples

--- ex8.py
+++ ex8.py
@@ -4,10 +4,15 @@ def pp(v: 'array', dim: 'intero', k: 'intero'):
 		v[dim-i-1] = 0
 		i=i+1
 
-vet = []
+"!hide"
+vet = [None] * 10
+"!show"
+"""!tex
+	\State{vet[10]}
+"""
 i=0
 while(i<10):
-	vet.append(25/4)
+	vet[i] = 25/4
 	i=i+1
 pp(vet, 10, 5)
 print("Lo array dopo pp è: ")
